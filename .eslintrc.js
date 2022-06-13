ecaadf9f9347b62098e9474c72656c0552789e72
SE2Dev <5873790+SE2Dev@users.noreply.github.com>
2022-06-13 15:30:31 -0400

    [config] Enable TypeScript Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,27 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "plugins": [
+        "@typescript-eslint"
+    ],
     "rules": {
-        "quotes": [
+        "@typescript-eslint/quotes": [
             "error",
             "double"
         ],
-        "semi": [
+        "@typescript-eslint/semi": [
             "error",
             "always"
+        ],
+        "@typescript-eslint/explicit-module-boundary-types": [
+            "error"
         ]
     }
 };
