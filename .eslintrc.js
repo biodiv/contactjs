bf35ba7da1f0853b84af87bbab9fa6e21a829ae5
SE2Dev <5873790+SE2Dev@users.noreply.github.com>
2022-06-13 15:22:36 -0400

    [config] Upgrade ESLint Configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
